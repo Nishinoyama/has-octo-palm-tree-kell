cc6f72d22ee8a0f1be82648326612daa5234f797
Nishinoyama <begoingto1414@gmail.com>
2022-08-20 18:56:17 +0900

    refactor: MergeSort

--- MergeSort.hs
+++ MergeSort.hs
@@ -2,13 +2,10 @@ mergeSort :: (Ord a) => [a] -> [a]
 
 mergeSort [] = []
 mergeSort [x] = [x]
-mergeSort x = merge sortedLeft sortedRight
+mergeSort x = mergeSort left `merge` mergeSort right
     where
-        sortedLeft = mergeSort left
-        sortedRight = mergeSort right
-        left = take half x
-        right = drop half x
-        half = length(x) `div` 2
+        (left, right) = splitAt half x
+        half = length x `div` 2
         merge x [] = x
         merge [] x = x
         merge (x:xs) (y:ys) = if x < y then x : merge xs (y : ys)  else y : merge (x : xs) ys
